eb89336eff1960b20278bd6a6b9bba875dca87ae
Cristian Monzon <cristian-02-@live.com>
2024-05-15 17:33:45 -0500

    Agregando todo

---
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gifs.interfaces';
+
+@Component({
+  selector: 'gifs-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrl: './card-list.component.css'
+})
+export class CardListComponent {
+  @Input()
+  public gifs: Gif[] = [];
+}
